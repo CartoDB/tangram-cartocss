7a63a857c23b53cd6abfc7d48f3cae71495ced0d
Francisco López <donflopez@gmail.com>
2016-12-22 18:23:22 +0100

    Add point tests

---
+++ test/basic/points.js
@@ -0,0 +1,143 @@
+/* globals describe, it */
+import Utils from '../utils/utils';
+import chai from 'chai';
+let assert = chai.assert;
+
+import Point from '../../src/basic/points';
+
+let pointCCSS = 
+` // Marker with global opacity and border
+  #layer {
+    marker-fill: #F00;
+    marker-width: 15px;
+    marker-line-width: 2px;
+    marker-line-color: #00F;
+
+    [height > 10] {
+      marker-width: 20px;
+      marker-line-width: 4px;
+      marker-opacity: 0.6;
+      marker-line-opacity: 0.6;
+    }
+
+    [height > 15] {
+      marker-width: 30px;
+      marker-line-width: 6px;
+      marker-opacity: 0.3;
+      marker-line-opacity: 0.3;
+    }
+
+  }
+`;
+
+describe('Point', () => {
+  const c3ss = Utils.getShader(pointCCSS);
+
+  describe('.getDraw()', () => {
+    let point = Point.getDraw(c3ss).points_blend;
+    
+    it('should have color', () => {
+      assert.property(point, 'color');
+    });
+
+    describe('.color: ', () => {
+      it('should be rgba(255, 0, 0, 1)', () => {
+        assert.equal(Utils.eval(point.color)({}, 10), 'rgba(255, 0, 0, 1)');
+      });
+
+      it('should be rgba(255, 0, 0, 0.6) with height > 10', () => {
+        assert.equal(Utils.eval(point.color)({height: 11}, 10), 'rgba(255, 0, 0, 0.6)');
+      });
+
+      it('should be rgba(255, 0, 0, 0.3) with height > 15', () => {
+        assert.equal(Utils.eval(point.color)({height: 16}, 10), 'rgba(255, 0, 0, 0.3)');
+      });
+    });
+    
+    it('should have size', () => {
+      assert.property(point, 'size');
+    });
+
+    describe('.size: ', () => {
+      it('should be 15', () => {
+        assert.equal(Utils.eval(point.size)({}, 10), '15');
+      });
+
+      it('should be 20 with height > 10', () => {
+        assert.equal(Utils.eval(point.size)({height: 11}, 10), '20');
+      });
+
+      it('should be 30 with height > 15', () => {
+        assert.equal(Utils.eval(point.size)({height: 16}, 10), '30');
+      });
+    });
+    
+    it('should have outline_width', () => {
+      assert.property(point, 'outline_width');
+    });
+
+    describe('.outline_width: ', () => {
+      it('should be 2', () => {
+        assert.equal(Utils.eval(point.outline_width)({}, 10), '2');
+      });
+
+      it('should be 4 with height > 10', () => {
+        assert.equal(Utils.eval(point.outline_width)({height: 11}, 10), '4');
+      });
+
+      it('should be 6 with height > 15', () => {
+        assert.equal(Utils.eval(point.outline_width)({height: 16}, 10), '6');
+      });
+    });
+
+    it('should have outline_color', () => {
+      assert.property(point, 'outline_color');
+    });
+
+    describe('.outline_color: ', () => {
+      it('should be rgba(0, 0, 255, 1)', () => {
+        assert.equal(Utils.eval(point.outline_color)({}, 10), 'rgba(0, 0, 255, 1)');
+      });
+
+      it('should be rgba(0, 0, 255, 0.6) with height > 10', () => {
+        assert.equal(Utils.eval(point.outline_color)({height: 11}, 10), 'rgba(0, 0, 255, 0.6)');
+      });
+
+      it('should be rgba(0, 0, 255, 0.3) with height > 15', () => {
+        assert.equal(Utils.eval(point.outline_color)({height: 16}, 10), 'rgba(0, 0, 255, 0.3)');
+      });
+    });
+
+    it('should not have collide', () => {
+      assert.notProperty(point, 'collide');
+    });
+
+
+  });
+
+  describe('.getStyle()', () => {
+    let point = Point.getStyle(c3ss);
+
+    it('should have points_blend property', () => {
+      assert.property(point, 'points_blend');
+    });
+
+    describe('.points_blend', () => {
+      it('should have base property', () => {
+        assert.property(point.points_blend, 'base');
+      });
+
+      it('shoul have blend property', () => {
+        assert.property(point.points_blend, 'blend');
+      });
+
+      it('should have base property equal to points', () => {
+        assert.equal(point.points_blend.base, 'points');
+      });
+
+      it('should have blend property equal to overlay', () => {
+        assert.equal(point.points_blend.blend, 'overlay');
+      });
+    });
+  });
+});
