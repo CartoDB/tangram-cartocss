2bd41666b8d37ba8efdbf93ed1efe61010e0b524
Raul Ochoa <rochoaf@gmail.com>
2017-08-31 10:35:05 +0000

    Ignore unused vars as they are used within the eval context

--- test/utils.js
+++ test/utils.js
@@ -3,11 +3,12 @@ const tangramReference = require('tangram-reference').load();
 function getReferenceDefaultMarkerValue(propertyName) {
     return tangramReference.symbolizers.markers[propertyName]['default-value'];
 }
+
 function evalFunctionString(functionString, feature) {
+    // jshint unused:false
+    // `feature` and `$zoom` variables are required within the eval context.
     const $zoom = 3;
-    const data = feature;
-    eval('var fn = ' + functionString + ';');
-    return fn();
+    return eval(`(${functionString})`)();
 }
 
 function evalIfNeeded(yamlProperty, feature) {
